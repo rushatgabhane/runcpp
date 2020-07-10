3c433767185bc182be2ef941ec3e170729e2bf9a
rushatgabhane <rushatgabhane@gmail.com>
2020-07-10 21:37:15 +0530

    check if extension is cpp

--- runcpp.sh
+++ runcpp.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-#path="$1"
-#filename="${path##*/}"
+# Check if g++ exists
+command -v g++ >/dev/null 2>&1 || {
+    echo >&2 "g++ is not installed. Please run sudo apt install g++";
+    exit 1;
+}
 
 usage(){
-cat <<EOF
-Usage: runcpp [arguments if any] <file.cpp>
-Arguments:
-   -h  or  --help	Print Help and exit
-EOF
+	cat <<EOF
+	Usage: runcpp [arguments if any] <file.cpp>
+	Arguments:
+   		-h  or  --help	Print Help and exit
+	EOF
 }
 
 # read args
@@ -21,17 +24,14 @@ case $1 in
 		;;
 esac
 
-# Check if g++ exists
-command -v g++ >/dev/null 2>&1 || {
-    echo >&2 "g++ is not installed. Please run sudo apt install g++";
-    exit 1;
-}
-
 filename="$(basename $filepath)"
 filenameWithoutExtension="$(basename $filepath .cpp)"
+extension="${filename#*.}"
+
+if [  "$extension" != "cpp" ]; then
+	usage
+	exit
+fi
 
 # compile and execute
 # g++ -std=c++14 -Wshadow -Wall -o "%e" "%filepath" -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG
-
-echo "$filename"
-echo "$filenameWithoutExtension"
